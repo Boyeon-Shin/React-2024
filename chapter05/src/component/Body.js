be853937004b4ccf150d5c989511b57aa13d4fe6
Boyeon-Shin <20221023@o365.hanbat.ac.kr>
2024-01-02 17:02:11 +0900

    Chapter05 study

---
+++ chapter05/src/component/Body.js
@@ -0,0 +1,38 @@
+import React, {useRef, useState} from "react";
+
+function Body() {
+    const [text, setText] = useState("");
+    const  [change, setChange] = useState ("");
+
+    const textRef = useRef();
+    const handleOnChange = (e) => {
+        setText(e.target.value);
+    };
+
+    const handleOnClick = () => {
+        if (text.length < 5) {
+            textRef.current.focus();
+            setChange("입력값을 정확하게 입력하세요");
+            setTimeout(() => setChange(""), 2000);
+
+        } else {
+            alert("입력 완료");
+            setText("");
+        }
+
+
+    };
+
+    return (
+        <div>
+            <input ref={textRef} value={text} onChange={handleOnChange}/>
+            <button onClick={handleOnClick}>작성완료</button>
+            <div  style ={{color: "red"}} >{change}</div>
+            <div>{text}</div>
+
+        </div>
+
+    );
+}
+
+export default Body;
