5cbd586faac79159383fa9ff68a40c0f3468bbf9
Boyeon-Shin <20221023@o365.hanbat.ac.kr>
2024-01-08 15:39:06 +0900

    chapter05 Body,js

--- chapter05/src/component/Body.js
+++ chapter05/src/component/Body.js
@@ -2,7 +2,8 @@ import React, {useRef, useState} from "react";
 
 function Body() {
     const [text, setText] = useState("");
-    const  [change, setChange] = useState ("");
+    // const  [change, setChange] = useState ("");
+    const [change, setChange]  = useState(false);
 
     const textRef = useRef();
     const handleOnChange = (e) => {
@@ -12,7 +13,8 @@ function Body() {
     const handleOnClick = () => {
         if (text.length < 5) {
             textRef.current.focus();
-            setChange("입력값을 정확하게 입력하세요");
+            // setChange("입력값을 정확하게 입력하세요");
+            setChange(true);
             setTimeout(() => setChange(""), 2000);
 
         } else {
@@ -27,7 +29,8 @@ function Body() {
         <div>
             <input ref={textRef} value={text} onChange={handleOnChange}/>
             <button onClick={handleOnClick}>작성완료</button>
-            <div  style ={{color: "red"}} >{change}</div>
+            {/*<div  style ={{color: "red"}} >{change}</div>*/}
+            {change ? <div style ={{color: "red"}}>"입력값을 정확하게 입력하세요"</div> : <></>}
             <div>{text}</div>
 
         </div>
