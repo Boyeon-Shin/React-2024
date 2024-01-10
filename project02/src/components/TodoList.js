dba0266b5211b2a39e7613542769b32987d90825
Boyeon-Shin <20221023@o365.hanbat.ac.kr>
2024-01-10 16:34:46 +0900

    project02

---
+++ project02/src/components/TodoList.js
@@ -0,0 +1,34 @@
+import "./TodoList.css";
+import TodoItem from "./TodoItem";
+import {useState} from "react";
+
+const TodoList = ({todo, onUpdate, onDelete}) => {
+    const [search, setSearch] = useState("");
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value);
+    };
+    const getSearchResult = () => {
+        return search === ""
+        ? todo
+        : todo.filter((it) =>
+                it.content.toLowerCase().includes(search.toLowerCase()));
+    };
+
+    return (
+        <div className= "TodoList">
+            <h4>Todo List 🌼</h4>
+            <input
+                value={search}
+                onChange={onChangeSearch}
+                className="searchbar"
+                placeholder="검색어를 입력하시오"/>
+
+            <div className= "list_wrapper">
+                {getSearchResult().map((it) => (
+                    <TodoItem key = {it.id} {...it} onUpdate = {onUpdate} onDelete ={onDelete} />
+                    ))}
+            </div>
+        </div>
+    );
+};
+export default TodoList;
