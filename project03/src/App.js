ccec4f7b14be31e0665a929e97386a928d2c2536
Boyeon-Shin <20221023@o365.hanbat.ac.kr>
2024-02-08 09:54:33 +0900

    project03 생성

---
+++ project03/src/App.js
@@ -0,0 +1,55 @@
+import './App.css';
+import Edit from "./pages/Edit";
+import {createBrowserRouter, Link, Route, RouterProvider, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+
+
+export const router = createBrowserRouter([
+    {
+        path: "",
+        element: <Home/>,
+    },
+    {
+        path: "/new",
+        element: (
+            <New/>
+        ),
+    },
+    {
+        path: "/diary/:id",
+        element:
+            <Diary/>,
+    },
+    {
+        path: "/edit/:id",
+        element: <Edit/>,
+    },
+]);
+
+function App() {
+  return (
+
+      // <div className="App">
+      //   <Routes>
+      //     <Route path='/' element={<Home/>}/>
+      //     <Route path='/new' element={<New/>}/>
+      //     <Route path='/diary/:id' element={<Diary/>}/>
+      //     <Route path='/edit' element={<Edit/>}/>
+      //   </Routes>
+      //   <div>
+      //     <Link to={"/"}>Home</Link>
+      //     <Link to={"/new"}>New</Link>
+      //     <Link to={"/diary"}>Diary</Link>
+      //     <Link to={"/edit"}>Edit</Link>
+      //   </div>
+
+          <RouterProvider router={router}>
+              <div className="App">
+              </div>
+          </RouterProvider>
+  );
+}
+
+export default App;
---
+++ project03/src/pages/Home.js
@@ -0,0 +1,12 @@
+import Button from "../components/Button";
+import Header from "../components/Header"
+import Editor from "../components/Editor";
+
+const Home = () => {
+    return (
+        <div>
+            <Editor />
+        </div>
+    );
+};
+export default Home;
